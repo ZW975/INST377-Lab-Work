cec29357118d46ac208f1ada3022c3ee704345d3
ZW975 <71403189+ZW975@users.noreply.github.com>
2022-09-30 01:05:14 -0400

    second build for submission 1

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -79,6 +79,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // down movement
   timerID = setInterval(moveDown, 1000);
 
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener('keyup', control);
+
   function freeze() {
     if (current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
@@ -96,5 +109,45 @@ document.addEventListener('DOMContentLoaded', () => {
     currentPosition += width;
     draw();
     freeze();
+  }
+
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some((index) => (currentPosition + index) % width === 0);
+
+    if (!isAtLeftEdge) {
+      currentPosition -= 1;
+    }
+
+    if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1;
+    }
+
+    draw();
+  }
+
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some((index) => (currentPosition + index) % width === width - 1);
+
+    if (!isAtRightEdge) {
+      currentPosition += 1;
+    }
+
+    if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
+
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    currentRotation += 1;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
   }
 });
